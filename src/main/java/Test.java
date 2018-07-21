248b489d09fae4cf4c436e84a2558971cab2481f
zhouqw520 <qstx520@163.com>
2018-07-21 20:03:31 +0800

    更新脚本

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,10 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Test!");
+        System.out.println("abc");
+    }
+
+    public void version(){
+        System.out.println("感谢！");
     }
 }
